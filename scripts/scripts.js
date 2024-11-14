d5498a0ead577bf67623bcbeded3e08eed0fb377
ekremney <ekdogan@adobe.com>
2024-11-14 17:25:58 +0100

    add bulks gtm container

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -222,16 +222,6 @@ export function addFavIcon(href) {
   }
 }
 
-export function createInlineScript(document, element, innerHTML, type) {
-  const script = document.createElement('script');
-  if (type) {
-    script.type = type;
-  }
-  script.innerHTML = innerHTML;
-  element.appendChild(script);
-  return script;
-}
-
 /**
  * loads everything that doesn't need to be delayed.
  */
@@ -264,12 +254,21 @@ async function loadLazy(doc) {
   sampleRUM.observe(main.querySelectorAll('div[data-block-name]'));
   sampleRUM.observe(main.querySelectorAll('picture > img'));
 
-  const GTM_LOAD_SCRIPT = `(function(w,d,s,l,i){var f=d.getElementsByTagName(s)[0],
+  document.head.prepend(document.createRange().createContextualFragment(`
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-MP28XD9M');`;
+  })(window,document,'script','dataLayer','GTM-MP28XD9M');</script>
+  <!-- End Google Tag Manager -->`));
 
-  createInlineScript(document, document.body, GTM_LOAD_SCRIPT, 'text/javascript');
+  document.body.prepend(document.createRange().createContextualFragment(`
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MP28XD9M"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  `));
 
   // Add below snippet at the end of the lazy phase
   if ((getMetadata('experiment')
